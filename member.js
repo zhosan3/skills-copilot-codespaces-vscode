b20ec913b6c3441bbe1da639a946f89a6606fc8c
zhosan3 <135704005+zhosan3@users.noreply.github.com>
2023-07-19 09:08:37 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/skills-member.html',
+    controller: 'SkillsMemberController',
+    controllerAs: 'skillsMemberCtrl'
+  };
+}
